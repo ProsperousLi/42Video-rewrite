096f10f60b61a327526c8e7a99781e5436798c75
ProsperousLi <806966854@qq.com>
2016-05-04 10:03:24 +0800

    Initial Commit

---
+++ 42Video-rewrite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  42Video-rewrite
+//
+//  Created by dragon on 16/5/4.
+//  Copyright © 2016年 李荣繁(LiDragon). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 42Video-rewrite/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  42Video-rewrite
+//
+//  Created by dragon on 16/5/4.
+//  Copyright © 2016年 李荣繁(LiDragon). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
